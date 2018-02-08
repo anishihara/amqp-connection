e340cc9999d3ab0ee6fa2fe0561d717eb4e89036
Anderson Nishihara <nishihara@gmail.com>
2018-02-08 16:24:48 +0000

    Fix bug: when there is an error on channel, disconnect and reconnect on the server.

--- service.js
+++ service.js
@@ -19,6 +19,8 @@ const publishOfflineMessage = () => {
 const handleChannelCreated = (ch) => {
     channel = ch;
     channel.prefetch(prefetchCount);
+    channel.on("error", onError);
+    channel.on("close", handleReconnection);
     return channel;
 }
 
@@ -33,32 +35,54 @@ const onError = (err) => {
     if (err.message !== "Connection closing") {
         console.error("[AMQP] conn error", err.message);
     }
+    handleReconnection();
 }
 const handleReconnection = () => {
     console.error("[AMQP] reconnecting");
     return setTimeout(reconnect, 5000);
 }
 
-const reconnect = () => {
+const cleanUp = () => {
+    if (channel) {
+        channel.removeListener('close', handleReconnection);
+        channel.removeListener('error',onError);
+        try {
+            channel.close();
+        } catch (err) {
+            console.error("[AMQP] Could not close the channel. Is it already closed?");
+        }
+    }
     if (connection) {
         connection.removeListener('close', handleReconnection);
+        connection.removeListener('error',onError);
+        try {
+            connection.close();
+        }
+        catch (err) {
+            console.error("[AMQP] Could not close the connection. Is it already closed?");
+        }
     }
+
     connection = null;
     channel = null;
-    connect(serverUri,prefetchCount);
+}
+
+const reconnect = () => {
+    cleanUp();
+    connect(serverUri, prefetchCount);
 }
 
 const reAttachMessageHandlers = () => {
     messageHandlersData.forEach(data => {
-        onMessage(data.exchange, data.queueName, data.routingKey, data.messageHandler,data.config);
+        onMessage(data.exchange, data.queueName, data.routingKey, data.messageHandler, data.config);
     });
     return Promise.resolve();
 }
 
-const connect = (server,prefetch=1) => {
-    if (prefetchCount!==prefetch) prefetchCount = prefetch;
+const connect = (server, prefetch = 1) => {
+    if (prefetchCount !== prefetch) prefetchCount = prefetch;
     if (!serverUri) serverUri = server;
-    
+
     if (connection) {
         if (channel) {
             return Promise.resolve(connection)
@@ -69,7 +93,7 @@ const connect = (server,prefetch=1) => {
         .then(handleChannelCreated)
         .then(publishOfflineMessage)
         .then(reAttachMessageHandlers)
-        .then(()=>{
+        .then(() => {
             return Promise.resolve(connection);
         })
         .catch(err => {
@@ -85,8 +109,8 @@ const disconnect = () => {
         connection.removeListener('close', handleReconnection);
         connection.close();
     }
-    messageHandlersData.length=0;
-    offlinePubQueue.length=0;
+    messageHandlersData.length = 0;
+    offlinePubQueue.length = 0;
     connection = null;
     channel = null;
 }
@@ -115,10 +139,10 @@ const send = (exchange, routingKey, message) => {
 
 
 const onMessage = (exchange, queueName, routingKey, messageHandler, queueConfigs) => {
-    const config = queueConfigs?queueConfigs: {
-        noAck:true,
-        autoDelete:false,
-        durable:true
+    const config = queueConfigs ? queueConfigs : {
+        noAck: true,
+        autoDelete: false,
+        durable: true
     }
     channel.assertQueue(queueName, config).then((q) => {
         channel.bindQueue(q.queue, exchange, routingKey);
@@ -126,12 +150,12 @@ const onMessage = (exchange, queueName, routingKey, messageHandler, queueConfigs
             if (typeof messageHandler.then == 'function') {
                 messageHandler(msg).then(r => { if (!config.noAck) channel.ack(msg); });
             }
-            else{
+            else {
                 messageHandler(msg);
                 if (!config.noAck) channel.ack(msg);
             }
         }, config);
-        if (!messageHandlersData.find(a => a.exchange === exchange && a.queueName === queueName && a.routingKey === routingKey && a.messageHandler === messageHandler && a.config === config)) messageHandlersData.push({ exchange,queueName, routingKey, messageHandler,config});
+        if (!messageHandlersData.find(a => a.exchange === exchange && a.queueName === queueName && a.routingKey === routingKey && a.messageHandler === messageHandler && a.config === config)) messageHandlersData.push({ exchange, queueName, routingKey, messageHandler, config });
     }).catch(err => { console.error(err) })
 }
 
